75058e6e85ba744b709b78a7c9539d67c6a3048f
miavrako <miavrako@student.42antananarivo.mg>
2026-03-10 16:01:46 +0300

    add total_operations 1sttry

--- bench.c
+++ bench.c
@@ -6,8 +6,23 @@
 /*   By: miavrako <miavrako@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 12:50:14 by miavrako          #+#    #+#             */
-/*   Updated: 2026/03/10 13:10:29 by miavrako         ###   ########.fr       */
+/*   Updated: 2026/03/10 16:01:13 by miavrako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	total_operations(t_stack *stack) // essaie pour calcul de ligne de base
+{
+	int total;
+
+	total = 0;
+	while (stack)
+	{
+		ra(stack);
+		total++;
+		rra(stack);
+		total++;
+	}
+	return (total);
+}
