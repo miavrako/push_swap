99315d366558864aa480d164137800f37513d1aa
miavrako <miavrako@student.42antananarivo.mg>
2026-03-11 09:26:45 +0300

    update medium

--- algo_medium.c
+++ algo_medium.c
@@ -6,36 +6,84 @@
 /*   By: miavrako <miavrako@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 11:38:32 by mirarand          #+#    #+#             */
-/*   Updated: 2026/03/10 19:12:00 by miavrako         ###   ########.fr       */
+/*   Updated: 2026/03/11 09:25:22 by miavrako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	find_max_pos(t_stack *stack)
+{
+	int	max;
+	int	pos;
+	int	i;
+
+	if (!stack)
+		return (0);
+	max = stack->index;
+	pos = 0;
+	i = 0;
+	while (stack)
+	{
+		if (stack->index > max)
+		{
+			max = stack->index;
+			pos = i;
+		}
+		stack = stack->next;
+		i++;
+	}
+	return (pos);
+}
+
+void	push_max_to_a(t_stack **a, t_stack **b)
+{
+	int	pos;
+	int	size;
+
+	pos = find_max_pos(*b);
+	size = stack_size(*b);
+
+	if (pos <= size / 2)
+		while (pos--)
+			rb(b);
+	else
+		while (pos++ < size)
+			rrb(b);
+
+	pa(a, b);
+}
+
 void	algo_medium(t_stack **stack_a, t_stack **stack_b)
 {
 	int	chunk;
-	int	index;
+	int	pushed;
 	int	size;
 
-	size = stack_size(*stack_a);
-	index = 0;
 	if (!stack_a || !*stack_a || is_sorted(*stack_a))
 		return ;
-	if (size == 100)
-		chunk = size / 15;
+	size = stack_size(*stack_a);
+	if (size <= 100)
+		chunk = 15;
 	else
-		chunk = size / 40;
+		chunk = 30;
+	pushed = 0;
 	while (*stack_a)
 	{
-		while (index++ < size)
+		if ((*stack_a)->index <= pushed)
 		{
-			if ((*stack_a)->index <= index + chunk)
-				pb(stack_a, stack_b);
-			else
-				ra(stack_a);
+			pb(stack_a, stack_b);
+			rb(stack_b);
+			pushed++;
 		}
+		else if ((*stack_a)->index <= pushed + chunk)
+		{
+			pb(stack_a, stack_b);
+			pushed++;
+		}
+		else
+			ra(stack_a);
 	}
 	while (*stack_b)
-		pa(stack_a, stack_b);
+		push_max_to_a(stack_a, stack_b);
 }
